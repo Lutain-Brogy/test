7f90477ae0961758475527eca0fcf713ec83f397
Lutain-Brogy <brogylutain@gmail.com>
2026-05-14 09:26:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
-
+# --- Firebase init from Streamlit secrets ---
 if not firebase_admin._apps:
+    cred = credentials.Certificate(st.secrets["firebase"])
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
+# --- UI ---
 st.title("My Firestore App")
 
 if st.button("Get Data"):
     docs = db.collection("users").stream()
     for d in docs:
         st.write(d.to_dict())
+        
