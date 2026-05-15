fab333ac28e3ebfa23cc8231afff5bf9fec5832a
Lutain-Brogy <brogylutain@gmail.com>
2026-05-15 07:17:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,23 @@ match /your_collection/{userId} {
 elif edit_choice == "Specific role reads":
     st.write("Copy rule below")
 
+    user_id = st.text_input("Please paste the user ID allowed to read")
+
+    if user_id:
+        st.code(f"""
+rules_version = '2';
+service cloud.firestore {{
+  match /databases/{{database}}/documents {{
+
+    match /your_collection/{{documentId}} {{
+      allow read: if request.auth != null
+                  && request.auth.uid == "{user_id}";
+    }}
+
+  }}
+}}
+""", language="javascript")
+        
 elif edit_choice == "Conditional data based read":
     st.write("Copy rule below")
 
